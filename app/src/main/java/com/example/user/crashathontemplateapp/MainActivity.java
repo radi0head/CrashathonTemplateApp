bc9b686f66f85ff44e39c9418eae840ebc1d93be
Subhanjan Saha <subhanjan.saha1998@gmail.com>
2018-11-28 12:41:21 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/user/crashathontemplateapp/MainActivity.java
@@ -0,0 +1,253 @@
+package com.example.user.crashathontemplateapp;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+    int count=0;
+    Button crash1=null;
+    Button crash2=null;
+    TextView scoreTextView=null;
+
+    @Override
+    public void onClick(View v) {
+        int id=v.getId();
+        if(id==R.id.crash1){
+            count++;
+            updateScore(count);
+            throw new RuntimeException("crash");
+        }else if(id==R.id.crash2){
+            count++;
+            updateScore(count);
+            throw new RuntimeException("crash");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+//        drawer.setVisibility(View.GONE);
+        crash1=(Button)findViewById(R.id.crash1);
+        crash2=(Button)findViewById(R.id.crash2);
+        crash1.setOnClickListener(this);
+        crash2.setOnClickListener(this);
+
+        String scoreString="";
+        File file = new File(this.getExternalFilesDir(null), "ScoreData");
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+            // Get the object of DataInputStream
+            DataInputStream in = new DataInputStream(fis);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            scoreString=br.readLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if(scoreString.isEmpty()){
+            updateScore(count);
+        }else{
+            count=readScore();
+        }
+
+        scoreTextView=(TextView)findViewById(R.id.score_text);
+        scoreTextView.setText(""+count);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public boolean updateScore(int count){
+        // check if available and not read only
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
+            Log.w("FileUtils", "Storage not available or read only");
+            return false;
+        }
+
+        // Create a path where we will place our List of objects on external storage
+        File file = new File(this.getExternalFilesDir(null), "ScoreData");
+        PrintStream p = null; // declare a print stream object
+        boolean success = false;
+
+        try {
+            OutputStream os = new FileOutputStream(file);
+            // Connect print stream to the output stream
+            p = new PrintStream(os);
+            p.println(""+count);
+            Log.w("FileUtils", "Writing file" + file);
+            success = true;
+        } catch (IOException e) {
+            Log.w("FileUtils", "Error writing " + file, e);
+        } catch (Exception e) {
+            Log.w("FileUtils", "Failed to save file", e);
+        } finally {
+            try {
+                if (null != p)
+                    p.close();
+            } catch (Exception ex) {
+            }
+        }
+
+        return success;
+    }
+
+    public int readScore(){
+
+        int score=0;
+
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly())
+        {
+            Log.w("FileUtils", "Storage not available or read only");
+            return 13;
+        }
+
+        FileInputStream fis = null;
+
+        try
+        {
+            File file = new File(this.getExternalFilesDir(null), "ScoreData");
+            fis = new FileInputStream(file);
+            // Get the object of DataInputStream
+            DataInputStream in = new DataInputStream(fis);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            //Read File Line By Line
+            while ((strLine = br.readLine()) != null) {
+                Log.w("FileUtils", "File data: " + strLine);
+                score=Integer.parseInt(""+strLine.charAt(0));
+            }
+            in.close();
+        }
+        catch (Exception ex) {
+            Log.e("FileUtils", "failed to load file", ex);
+        }
+        finally {
+            try {if (null != fis) fis.close();} catch (IOException ex) {}
+        }
+
+        return score;
+
+    }
+
+    private static boolean isExternalStorageReadOnly() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isExternalStorageAvailable() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+}
